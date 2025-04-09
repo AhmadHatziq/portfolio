cb25b74684610110a09c17f97ffa0c8df573e64a
Hatziq <hatziq@u.nus.edu>
2025-04-09 17:57:16 +0800

    change to hash router

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { Route, Switch, BrowserRouter } from "react-router-dom";
+import { Route, Switch, BrowserRouter, HashRouter } from "react-router-dom";
+
 import Home from "../pages/home/HomeComponent";
 import Splash from "../pages/splash/Splash";
 import Education from "../pages/education/EducationComponent";
@@ -13,7 +14,7 @@ import Error404 from "../pages/errors/error404/Error";
 export default class Main extends Component {
   render() {
     return (
-      <BrowserRouter basename="/portfolio">
+      <HashRouter basename="/portfolio">
         <Switch>
           <Route
             path="/"
@@ -70,7 +71,7 @@ export default class Main extends Component {
             render={(props) => <Error404 {...props} theme={this.props.theme} />}
           />
         </Switch>
-      </BrowserRouter>
+      </HashRouter>
     );
   }
 }
